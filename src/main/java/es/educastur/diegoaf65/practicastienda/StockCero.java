9d0dea7d9f72b7c9df3d9b49db6d5a4194922cc9
1dawd21 <1dawd21@A204PC21.desarrolloweb.local>
2026-01-29 14:22:11 +0100

    Tienda2026 (29/01/2026)

---
+++ src/main/java/es/educastur/diegoaf65/practicastienda/StockCero.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.diegoaf65.practicastienda;
+
+/**
+ *
+ * @author 1dawd21
+ */
+public class StockCero extends Exception{
+    public StockCero(String mensaje){
+        super(mensaje);
+    }
+}
---
+++ src/main/java/es/educastur/diegoaf65/practicastienda/StockInsuficiente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.diegoaf65.practicastienda;
+
+/**
+ *
+ * @author 1dawd21
+ */
+public class StockInsuficiente extends Exception{
+    public StockInsuficiente(String mensaje){
+        super(mensaje);
+    }
+}
